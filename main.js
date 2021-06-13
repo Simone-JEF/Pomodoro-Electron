f49f3d070075ece178627f59018388efd7a1afd3
Simon <simone.liberti@jefnapoli.com>
2021-06-13 17:29:31 +0200

    packaged app with installer

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
 const { app, BrowserWindow } = require('electron')
-require('electron-reload')(__dirname, {
-    electron: require(`${__dirname}/node_modules/electron`)
-})
 
 function createWindow(){
     const win = new BrowserWindow({
